c02278610f83fb5585e36c09c693eb37631e3807
CHxCOOH <chxcooh@googlemail.com>
2023-11-20 01:02:59 +0800

    🐛 fix tag

--- platforms/pixiv.py
+++ platforms/pixiv.py
@@ -88,7 +88,7 @@ async def get_artworks(
     # if r18:
     #     tags.add("#NSFW")
     if illust["illust_ai_type"]:
-        tags.add("AI")
+        tags.add("#AI")
 
     meta_pages = illust["meta_pages"]
     for i in range(page_count):
