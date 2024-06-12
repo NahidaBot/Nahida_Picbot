88bfe7d18348e1af3024c1815d1c2bf706ae14c7
Nahida <53059854+NahidaBuer@users.noreply.github.com>
2024-06-13 07:01:35 +0800

    :thread: refactored image downloading, now it's asynchronous

--- platforms/default.py
+++ platforms/default.py
@@ -1,18 +1,16 @@
-import requests
+import asyncio
 import json
 import os
 import logging
 import subprocess
-
-from retry import retry
-from sqlalchemy import func
+from typing import Any
 
 from telegram import User
 
 from config import config
 from entities import Image, ImageTag, ArtworkResult
 from utils.escaper import html_esc
-from utils import check_deduplication_via_url
+from utils import check_duplication_via_url
 from db import session
 
 logger = logging.getLogger(__name__)
@@ -50,83 +48,114 @@ class DefaultPlatform:
             raise GetArtInfoError(f"获取 {cls.platform} 平台图片出错\！")
     
     @classmethod
+    async def check_duplication(cls, artwork_info: list[list], user: User, post_mode: bool) -> ArtworkResult:
+        if post_mode and config.bot_deduplication_mode:
+            existing_image = check_duplication_via_url(artwork_info[1][1])
+            if existing_image:
+                logger.warning(f"试图发送重复的图片: {cls.platform}" + existing_image)
+                user = User(existing_image.userid, existing_image.username, is_bot=False)
+                return ArtworkResult(
+                    False,
+                    f"该图片已经由 {user.mention_html()} 于 {str(existing_image.create_time)[:-7]} 发过"
+                )
+        return ArtworkResult(True)
+    
+    @classmethod
+    async def get_images(
+        cls, 
+        user: User, 
+        post_mode, 
+        page_count, 
+        artwork_info: list[list], 
+        artwork_meta: dict[str, Any], 
+        artwork_result: ArtworkResult
+    ) -> list[Image]:
+        images = []
+        for i in range(page_count):
+            image = artwork_info[i+1]
+            if image[0] == 3:
+                image_info: dict = image[2]
+                extension = artwork_meta.get("extension") or artwork_meta.get("file_ext")
+                img = Image(
+                    userid=user.id,
+                    username=user.name,
+                    platform=cls.platform,
+                    pid=artwork_meta.get("id") or artwork_meta.get("media_id"),
+                    title=artwork_meta.get("title") or artwork_meta.get("tweet_content") or artwork_meta.get("id"),
+                    page=(i+1),
+                    author=artwork_meta.get("author"),
+                    authorid=(
+                        artwork_meta.get("pixiv_id")
+                        or artwork_meta.get("uploader_id") 
+                        or artwork_meta.get("approver_id") 
+                        or artwork_meta.get("creator_id") 
+                        or (artwork_meta.get("user") and artwork_meta.get("user").get("id"))
+                    ),
+                    r18=artwork_result.is_NSFW,
+                    extension=extension or image_info.get("extension") or image_info.get("file_ext"),
+                    size=image_info.get("file_size"),
+                    url_original_pic=image[1],
+                    url_thumb_pic=image_info.get("jpeg_url") or image_info.get("sample_url") or image[1],
+                    post_by_guest=(not post_mode),
+                    width=image_info.get("width") or image_info.get("image_width"),
+                    height=image_info.get("height") or image_info.get("image_height"),
+                    ai=artwork_result.is_AIGC,
+                    full_info=json.dumps(image_info),
+                )
+                img.filename = f"{img.pid}_{img.page}.{img.extension}"
+                images.append(img)
+                # img.size = os.path.getsize(file_path)
+                # r = requests.get(img.url_original_pic)
+                # file_path = cls.download_path + filename
+                # with open(file_path, "wb") as f:
+                #     f.write(r.content)
+                session.add(img)
+                artwork_result.feedback += f'第{i+1}张图片：{img.width}x{img.height}\n'
+        return images
+
+    @classmethod
+    async def download_image(cls, image: Image) -> None:
+        import httpx
+        async with httpx.AsyncClient(http2=True) as client:
+            response = await client.get(image.url_original_pic)
+            response.raise_for_status()
+            file_path = cls.download_path + image.filename
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+            logger.debug(f"已下载：{image.filename}")
+            if not image.size:
+                image.size = os.path.getsize(file_path)
+
+    @classmethod
     async def get_artworks(
         cls, url: str, input_tags: list, user: User, post_mode: bool = True
     ) -> ArtworkResult:
         try:
             artwork_info = await cls.get_info_from_gallery_dl(url)
 
-            if post_mode and config.bot_deduplication_mode:
-                existing_image = check_deduplication_via_url(artwork_info[1][1])
-                if existing_image:
-                    logger.warning(f"试图发送重复的图片: {cls.platform}" + existing_image)
-                    user = User(existing_image.userid, existing_image.username, is_bot=False)
-                    return ArtworkResult(
-                        False,
-                        f"该图片已经由 {user.mention_html()} 于 {str(existing_image.create_time)[:-7]} 发过"
-                    )
+            artwork_result = await cls.check_duplication(artwork_info, user, post_mode)
+            if not artwork_result.success:
+                return artwork_result
 
             page_count = len(artwork_info) - 1
-            msg = f"""获取成功！\n共有{page_count}张图片\n"""
+            artwork_result.feedback = f"""获取成功！\n共有{page_count}张图片\n"""
 
-            artwork_result = await cls.get_tags(input_tags, artwork_info)
+            artwork_result = await cls.get_tags(input_tags, artwork_info, artwork_result)
             artwork_meta: dict = artwork_info[0][-1]
-            images = []
+            artwork_result.images = await cls.get_images(user, post_mode, page_count, artwork_info, artwork_meta, artwork_result)
 
-            for i in range(page_count):
-                image = artwork_info[i+1]
-                if image[0] == 3:
-                    image_info: dict = image[2]
-                    extension = artwork_meta.get("extension") or artwork_meta.get("file_ext")
-                    img = Image(
-                        userid=user.id,
-                        username=user.name,
-                        platform=cls.platform,
-                        pid=artwork_meta.get("id"),
-                        title=artwork_meta.get("title") or artwork_meta.get("tweet_content") or artwork_meta.get("id"),
-                        page=(i+1),
-                        author=artwork_meta.get("author"),
-                        authorid=(
-                            artwork_meta.get("pixiv_id")
-                            or artwork_meta.get("uploader_id") 
-                            or artwork_meta.get("approver_id") 
-                            or artwork_meta.get("creator_id") 
-                            or artwork_meta.get("creator_id") 
-                            or artwork_meta.get("creator_id") 
-                            or artwork_meta.get("user").get("id")
-                        ),
-                        r18=artwork_result.is_NSFW,
-                        extension=extension or image_info.get("extension") or image_info.get("file_ext"),
-                        url_original_pic=image[1],
-                        url_thumb_pic=image_info.get("jpeg_url") or image_info.get("sample_url") or image[1],
-                        post_by_guest=(not post_mode),
-                        width=image_info.get("width") or image_info.get("image_width"),
-                        height=image_info.get("height") or image_info.get("image_height"),
-                        ai=artwork_result.is_AIGC,
-                        full_info=json.dumps(image_info),
-                    )
-                    images.append(img)
-                    r = requests.get(img.url_original_pic)
-                    filename = f"{img.pid}_{img.page}.{extension}"
-                    file_path = cls.download_path + filename
-                    with open(file_path, "wb") as f:
-                        f.write(r.content)
-                    img.size = os.path.getsize(file_path)
-                    img.filename = filename
-                    session.add(img)
-                    msg += f'第{i+1}张图片：{img.width}x{img.height}\n'
+            tasks = [asyncio.create_task(cls.download_image(image)) for image in artwork_result.images]
+            await asyncio.wait(tasks)
+            
             session.commit()
-            artwork_result.feedback = msg
-            artwork_result.images = images
-            artwork_result.success = True
             cls.get_caption(artwork_result, artwork_meta)
-
+            artwork_result.success = True
             return artwork_result
         except:
-            pass
+            return ArtworkResult(False, "呜呜呜，对不起主人喵，没能成功获取到图片")
 
     @classmethod
-    def get_caption(cls, artwork_result: ArtworkResult, artwork_meta: list[list]) -> ArtworkResult:
+    def get_caption(cls, artwork_result: ArtworkResult, artwork_meta: list[list]) -> None:
         caption = ''
         if artwork_meta.get("title"):
             caption += f"<blockquote>{html_esc(artwork_meta.get("title"))}</blockquote>\n"
@@ -138,12 +167,14 @@ class DefaultPlatform:
         
 
     @classmethod
-    async def get_tags(cls, input_tags: list[str], artwork_info: list[list]) -> ArtworkResult:
+    async def get_tags(cls, input_tags: list[str], artwork_info: list[list], artwork_result: ArtworkResult) -> ArtworkResult:
         artwork_meta: dict = artwork_info[0][1]
         raw_tags: list[str] = artwork_meta.get("tags", [])
+        raw_tags += artwork_meta.get("characters", [])
+        raw_tags += artwork_meta.get("artist", [])
+        raw_tags += artwork_meta.get("type", [])
         if isinstance(raw_tags, str):
             raw_tags = raw_tags.split()
-        artwork_result = ArtworkResult()
         
         input_set = set()
         for tag in input_tags:
@@ -159,6 +190,7 @@ class DefaultPlatform:
             # if len(tag.split()) > 3:
             #     raw_tags.append(tag.split())
             tag = tag.replace(" ", "_")
+            tag = tag.replace("-", "_")
             raw_tags_set.add("#" + html_esc(tag.lstrip("#")))
             
         all_tags = input_set & raw_tags_set
