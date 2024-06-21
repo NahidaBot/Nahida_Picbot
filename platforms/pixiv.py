33ed753c0e144196fb9f3866a7a642aafd3409eb
Nahida <53059854+NahidaBuer@users.noreply.github.com>
2024-06-21 10:54:37 +0800

    ✨ add english tags

--- platforms/pixiv.py
+++ platforms/pixiv.py
@@ -6,8 +6,6 @@ import json
 from datetime import datetime
 from typing import Any, Optional, Union
 
-# from pixivpy3 import *
-# from retry import retry
 from telegram import User
 import httpx
 
@@ -48,11 +46,12 @@ class Pixiv(DefaultPlatform):
             cls.headers["Accept-Language"] = "en"
         else:
             cls.headers["Accept-Language"] = "zh-CN,zh;q=0.9"
-
+        
         async with httpx.AsyncClient(http2=True) as client:
             response = await client.get(url, cookies=cls.cookies, headers=cls.headers)
             response.raise_for_status()
             j: dict[str, Any] = response.json()
+            logger.debug(j)
             return j["body"]
 
     @classmethod
@@ -96,15 +95,15 @@ class Pixiv(DefaultPlatform):
         https://www.pixiv.net/en/artworks/123456
         https://www.pixiv.net/member_illust.php?mode=medium&illust_id=123456
         """
-        import re
-
         reg = re.compile(
-            r"^(https?:\/\/)?(?:www\.)?(?:pixiv\.net\/(?:en\/)?(?:(?:i|artworks)\/|member_illust\.php\?(?:mode=[a-z_]*&)?illust_id=))?(\d+)$"
+            r"^(?:https?:\/\/)?(?:www\.)?(?:pixiv\.net\/(?:en\/)?(?:(?:i|artworks)\/|member_illust\.php\?(?:mode=[a-z_]*&)?illust_id=))?(\d+)$"
         )
         try:
             pid: int = int(reg.split(url)[1])
 
-            artwork_meta = await cls.get_info_from_web_api(pid)
+            artwork_meta, artwork_meta_en = await asyncio.gather(
+                cls.get_info_from_web_api(pid), cls.get_info_from_web_api(pid, "en")
+            )
 
             artwork_result = await cls.check_duplication(pid, user, post_mode)
             if not artwork_result.success:
@@ -123,19 +122,25 @@ class Pixiv(DefaultPlatform):
             artwork_result = await cls.get_tags(
                 input_tags, artwork_meta, artwork_result
             )
+            artwork_result = await cls.get_en_tags(
+                input_tags, artwork_meta_en, artwork_result
+            )
 
             tasks = [
-                asyncio.create_task(cls.download_image(image))
+                asyncio.create_task(
+                    cls.download_image(image, refer="https://www.pixiv.net/")
+                )
                 for image in artwork_result.images
             ]
             await asyncio.wait(tasks)
 
-            session.commit()
+            # session.commit() # 移至 command handler 发出 Image Group 之后
             artwork_result = cls.get_caption(artwork_result, artwork_meta)
             artwork_result.success = True
             return artwork_result
 
-        except:
+        except Exception as e:
+            logger.error(e)
             return ArtworkResult(
                 False, "出错了呜呜呜，对不起主人喵，没能成功获取到图片"
             )
@@ -170,7 +175,7 @@ class Pixiv(DefaultPlatform):
                 extension=urls["original"].split(".")[-1],
                 url_original_pic=urls["original"],
                 url_thumb_pic=urls["regular"],
-                r18=artwork_result.is_NSFW or artwork_meta["restrict"],
+                r18=bool(artwork_result.is_NSFW or artwork_meta["restrict"]),
                 width=image_info["width"],
                 height=image_info["height"],
                 post_by_guest=(not post_mode),
@@ -193,16 +198,16 @@ class Pixiv(DefaultPlatform):
         description: str = artwork_meta["description"]
         uploadDate: datetime = datetime.fromisoformat((artwork_meta["uploadDate"]))
 
-        is_title_included = title in artwork_result.tags
+        title_sharp = '#'+title
+        is_title_included = (title_sharp in artwork_result.raw_tags) or (title_sharp in artwork_result.tags)
+        title = f"<b>{html_esc((title))}</b>\n" if not is_title_included else ""
         artwork_result.caption = (
-            f"<b>{html_esc((title))}</b>\n"
-            if not is_title_included
-            else ""
+            f"{title}"
             f'<a href="https://pixiv.net/artworks/{pid}">Source</a>'
             f' by <a href="https://pixiv.net/users/{artwork_meta["userId"]}">Pixiv {artwork_meta["userName"]}</a>\n'
-            f'{" ".join(artwork_result.tags)}\n'
-            f"{uploadDate}"
-            f"<blockquote expandable>Raw Tags: {' '.join(artwork_result.raw_tags)}\n{description}</blockquote>\n"
+            f'{" ".join(artwork_result.tags)+"\n" if artwork_result.tags else ""}'
+            f"<blockquote expandable>{description+'\n' if description else ''}"
+            f"Raw Tags: {' '.join(artwork_result.raw_tags)}\n{uploadDate.strftime('%Y-%m-%d %H:%M:%S')}</blockquote>\n"
         )
         logger.debug(artwork_result)
         return artwork_result
@@ -220,21 +225,25 @@ class Pixiv(DefaultPlatform):
         # 这是为了解决一些中文标签被翻译成英文的问题, 如 原神 被翻译为 Genshin Impact
         # 能代(アズールレーン) Noshiro (Azur Lane) 也会使用原标签
         # 但是如果原标签里没有中文则依然会使用翻译后的标签, 如 フラミンゴ flamingo
-        raw_tags: list[dict[str, Any]] = artwork_meta["tags"]
+        raw_tags: list[dict[str, Any]] = artwork_meta["tags"]["tags"]
         tags_translated: list[str] = []
         tags_all: list[str] = []
         CHINESE_REGEXP = "[一-龥]"
         PUNCTUATION_PATTERN = r"""[!"$%&'()*+,-./:;<=>?@[\]^`{|}~．！？｡。＂＃＄％＆＇（）＊＋, －／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､　、〃〈〉《》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏﹑﹔·]"""
         for tag in raw_tags:
-            if "users入り" in tag["tag"]:
+            tag_raw = tag["tag"]
+            if "users入り" in tag_raw:
                 continue
-            tags_all.append(html_esc((tag["tag"])))
-            use_origin_tag = False
-            if tag["translation"]["en"].isascii():
-                if re.match(CHINESE_REGEXP, tag["tag"]):
-                    use_origin_tag = True
+            tag_raw = re.sub(PUNCTUATION_PATTERN, "", tag_raw)
+            tags_all.append("#" + html_esc((tag_raw)))
+            use_origin_tag = True
+            if tag.get("translation"):
+                use_origin_tag = False
+                if tag["translation"]["en"].isascii():
+                    if re.match(CHINESE_REGEXP, tag_raw):
+                        use_origin_tag = True
             tag_translated: str = (
-                tag["tag"] if use_origin_tag else tag["translation"]["en"]
+                tag_raw if use_origin_tag else tag["translation"]["en"]
             )
             tag_translated = tag_translated.replace(" ", "_")
             tag_translated = re.sub(PUNCTUATION_PATTERN, "", tag_translated)
@@ -259,24 +268,31 @@ class Pixiv(DefaultPlatform):
         logger.debug(artwork_result)
         return artwork_result
 
+    @classmethod
+    async def get_en_tags(
+        cls,
+        _: list[str],
+        artwork_meta: dict[str, Any],
+        artwork_result: ArtworkResult,
+    ) -> ArtworkResult:
+        raw_tags: list[dict[str, Any]] = artwork_meta["tags"]["tags"]
+        tags_en: list[str] = []
+        PUNCTUATION_PATTERN = r"""[!"$%&'()*+,-./:;<=>?@[\]^`{|}~．！？｡。＂＃＄％＆＇（）＊＋, －／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､　、〃〈〉《》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏﹑﹔·]"""
+        for tag in raw_tags:
+            tag_raw: str = tag["tag"]
+            if "users入り" in tag_raw:
+                continue
+            use_origin_tag = True
+            if tag.get("translation") and tag["translation"]["en"].isascii():
+                use_origin_tag = False
+            tag_en: str = (
+                tag_raw if use_origin_tag else tag["translation"]["en"]
+            )
+            tag_en = tag_en.replace(" ", "_").replace("-", "_")
+            tag_en = re.sub(PUNCTUATION_PATTERN, "", tag_en)
+            tags_en.append("#" + html_esc(tag_en))
 
-# api = AppPixivAPI()
-# api.set_accept_language("zh-cn")
-# api.auth(refresh_token=config.pixiv_refresh_token)
+        artwork_result.raw_tags = list(set(tags_en + artwork_result.raw_tags))
 
-
-# def refresh_token() -> None:
-#     api.auth(refresh_token=config.pixiv_refresh_token)
-#     sleep(1)
-
-
-# @retry(tries=3)
-# def get_illust(pid: int | str) -> dict:
-#     try:
-#         illust = api.illust_detail(pid)["illust"]
-#         logger.debug(illust)
-#         return illust
-#     except Exception as e:
-#         logger.error("获取失败, 可能是 Pixiv_refresh_token 过期, 正在尝试刷新")
-#         refresh_token()
-#         raise e
+        logger.debug(artwork_result)
+        return artwork_result
